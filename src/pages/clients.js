a32bcbf802e69d72f1dcacb06f4a7c3119d2a5ff
Julien Moreau <julien.moreau@belighted.com>
2020-03-25 12:07:14 +0100

    client page title

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import CasesList from "../components/cases/CasesList";
+import { SectionHeader } from "../components/sections";
 
 const ClientsPage = ({ pageContext }) => {
   return (
     <Layout context={pageContext} page={"clients"}>
-      <div className="o-wrapper">
-        <h1 className="c-heading c-heading--1 u-padding-vertical-large">Clients</h1>
+      <div className="o-wrapper c-section">
+        <SectionHeader
+          title={"Clients"}
+          body={
+            "Leading brands in Belgium and in Europe trust us and rely on Belighted development to deliver perfection every day."
+          }
+        ></SectionHeader>
         <CasesList />
       </div>
     </Layout>
