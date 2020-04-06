894fceeafee17803f91bfc67375d246ded6c241d
Julien Moreau <julien.moreau@belighted.com>
2020-04-06 12:18:29 +0200

    add active class to menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 import locales from "../../constants/locales";
+import classNames from 'classnames';
 
 const MenuLink = ({ name, lang }) => {
   return (
-    <Link to={`${locales[lang].path}/${name}`} className={"c-site-nav__item"}>
+    <Link
+      to={`${locales[lang].path}/${name}`}
+      className={classNames("c-site-nav__item", "c-site-nav-link")}
+      activeClassName={"active"}
+      partiallyActive={true}
+    >
       {name}
     </Link>
   );
