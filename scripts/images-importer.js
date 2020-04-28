a234bf5571c68087734d549124d73ca59e2594d1
Julien Moreau <julien@thylo.be>
2020-04-28 16:13:21 +0200

    BW-104 create script to import images

--- scripts/images-importer.js
+++ scripts/images-importer.js
@@ -2,53 +2,64 @@ const findInFiles = require("find-in-files");
 const path = require("path");
 const download = require("download");
 const nanoid = require("nanoid");
-const got = require("got");
 const FileType = require("file-type");
+const fs = require("fs");
+const del = require("del");
 
-findInFiles
-  .find(
-    new RegExp(
-      "https?:\\/\\/(www)?.belighted.com/.+\\.(png|jpg|svg|jpeg|webp)+"
-    ),
-    path.join(__dirname, "..", "content"),
-    ".(mdx*|yml)$"
-  )
-  .then(async results => {
-    console.log("found", Object.keys(results).length);
-    await Promise.all(
-      Object.keys(results).map(async key => {
-        const result = results[key];
-        const match = result.matches[0];
-        try {
-          const stream = download(match);
-          const { ext } = await FileType.fromStream(stream);
+const images = path.join(
+  "..",
+  "content",
+  "images",
+  "legacy"
+)(async () => {
+  const deletedPaths = await del([images]);
 
-          const newPath = path.join(
-            "..",
-            "content",
-            "images",
-            "legacy",
-            `${nanoid}.${ext}`
-          );
-          stream.pipe(fs.createWriteStream(newPath));
+  console.log("Deleted files and directories:\n", deletedPaths.join("\n"));
 
-          const file = fs.readFileSync(key, "utf-8");
-          fs.writeFileSync(
-            key,
-            file.replace(match, `/images/legacy/${newPath}`)
-          );
-          console.log(
-            'found "' + match + '" ' + result.count + ' times in "' + key + '"'
-          );
-        } catch (e) {
-          stream.destroy();
-          fs.writeFileSync(
-            key,
-            file.replace(match, "https://placekeanu.com/200/150")
-          );
-        }
-      })
-    );
-    console.log("done");
-  })
-  .catch(e => console.error(e));
+  findInFiles
+    .find(
+      new RegExp(
+        "https?:\\/\\/(www)?.belighted.com/.+\\.(png|jpg|svg|jpeg|webp)+"
+      ),
+      path.join(__dirname, "..", "content"),
+      ".(mdx*|yml)$"
+    )
+    .then(async results => {
+      console.log("found", Object.keys(results).length);
+      await Promise.all(
+        Object.keys(results).map(async key => {
+          const result = results[key];
+          const match = result.matches[0];
+          try {
+            const stream = download(match);
+            const { ext } = await FileType.fromStream(stream);
+
+            const newPath = path.join(images, `${nanoid}.${ext}`);
+            stream.pipe(fs.createWriteStream(newPath));
+
+            const file = fs.readFileSync(key, "utf-8");
+            fs.writeFileSync(
+              key,
+              file.replace(match, `/images/legacy/${newPath}`)
+            );
+            console.log(
+              'found "' +
+                match +
+                '" ' +
+                result.count +
+                ' times in "' +
+                key +
+                '"'
+            );
+          } catch (e) {
+            fs.writeFileSync(
+              key,
+              file.replace(match, "https://placekeanu.com/200/150")
+            );
+          }
+        })
+      );
+      console.log("done");
+    })
+    .catch(e => console.error(e));
+})();
