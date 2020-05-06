942d8c4294b14cf129dcba7c1d9415410a63c101
Julien Moreau <julien@thylo.be>
2020-05-06 08:30:27 +0200

    BW-120 Add process subtitle

--- src/components/processes/HomeProcessesList.js
+++ src/components/processes/HomeProcessesList.js
@@ -13,6 +13,7 @@ const HomeProcessesList = () => {
           lang
           slug
           title
+          subtitle
           intro
           button {
             label
@@ -34,11 +35,8 @@ const HomeProcessesList = () => {
             <span className="c-heading c-heading--1">{index + 1}</span>
             <span className="c-heading c-heading--1 u-color-brand-5">.</span>
             <h4 className={"c-heading c-heading--3"}>{node.title}</h4>
-            <h5 className={"c-heading c-heading--5"}>
-              <strong>{node.objective}</strong>
-              <br />
-              {node.duration}
-            </h5>
+            <h4 className={"c-heading c-heading--4"}>{node.subtitle}</h4>
+
             <div>{node.intro}</div>
 
             <p className={"u-margin-top"}>
@@ -46,6 +44,12 @@ const HomeProcessesList = () => {
                 {node.button.label}
               </LinkToService>
             </p>
+
+            <h5 className={"c-heading c-heading--5"}>
+              <strong>{node.objective}</strong>
+              <br />
+              {node.duration}
+            </h5>
           </li>
         ))}
     </ol>
